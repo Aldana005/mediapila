ce89ff238b1e1cc4e6a6337a1153d877425159a7
Aldana005 <aldanamariel005@gmail.com>
2025-12-03 15:55:02 -0300

    Add files via upload

---
+++ Login-ald.js
@@ -0,0 +1,48 @@
+// ref a elementos del form
+const emailInput = document.querySelector("#emailInput");
+const passwordInput = document.querySelector("#passwordInput");
+const boton = document.querySelector("#btnIngresar");
+const error_mensaje = document.querySelector("#error_mensaje");
+
+const users = [
+    {
+        nombre: "Aldana",
+        apellido: "Sánchez",
+        correo: "aldanamariel005@gmail.com",
+        contrasena: "grupo6.1",
+        token: "tokenaldana"
+    }
+    
+];
+
+init();
+
+function init() {
+    const token = localStorage.getItem("UsuarioAldana");
+    if (token) {
+        //direcciona a la aplicacion 
+        window.location = "app.html";
+    }
+}
+
+boton.addEventListener("click", function() {
+    
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    
+    const queryUser = users.find((user) => user.correo === email);
+
+    if (queryUser && queryUser.contrasena === password) {
+        
+
+        localStorage.setItem("UsuarioAldana", queryUser.token);
+        window.location = "app.html";
+
+    } else {
+        error_mensaje.textContent = "El usuario no existe";
+        error_mensaje.style.color = "red";
+        setTimeout(() => {
+            error_mensaje.textContent = "";
+        }, 5000);
+    }
+});
