a3ef33e4c05603a7926d07dffac21505c5bf21a7
Aldana005 <aldanamariel005@gmail.com>
2025-12-10 16:41:34 -0300

    Update cerrarSesion.js

--- cerrarSesion.js
+++ cerrarSesion.js
@@ -1,7 +1,16 @@
-const cerrar_sesion = document.querySelector("#btnCerrarSesion")
+// Validación de acceso -> no podes entrar a app.html sin validar mail y contraseña
+const token = localStorage.getItem("irupeApp");
+if (!token) {
+    window.location.href = "login.html";
+}
 
-cerrar_sesion.addEventListener('click', () => {
-    console.log('HOLA')
-    localStorage.removeItem('irupeApp')
 
-window.location.href = "login.html";})
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cerrar_sesion = document.querySelector("#btnCerrarSesion");
+
+    cerrar_sesion.addEventListener('click', () => {
+        localStorage.removeItem('irupeApp');
+        window.location.href = "login.html";
+    });
+});
