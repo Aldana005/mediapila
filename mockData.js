104b8d385120c3466bcc85d93ab685643f171f56
cepedasilvanagabriela <cepedasilvanagabriela@gmail.com>
2025-12-15 14:01:57 -0300

    Update mockData.js

--- mockData.js
+++ mockData.js
@@ -3,61 +3,70 @@ const tareasMock = [
         id: 1,
         nombre: "Clase de Programación",
         fecha: "2025-12-11",
-        prioridad: "alta"
+        prioridad: "alta",
+        realizada: false,
     },
     {
         id: 2,
         nombre: "Hacer limpieza general en casa",
         fecha: "2025-12-12",
-        prioridad: "media"
-        
+        prioridad: "media",
+        realizada: false,        
     },
     {
         id: 3,
         nombre: "Hacer la comida para Navidad",
         fecha: "2025-12-23",
-        prioridad: "media"
+        prioridad: "media",
+        realizada: false,
     },
     {
         id: 4,
         nombre: "Organizar archivos del curso",
         fecha: "2025-12-14",
-        prioridad: "baja"
+        prioridad: "baja",
+        realizada: false,
     },
     {
         id: 5,
         nombre: "Corregir errores del trabajo Grupal",
         fecha: "2025-12-15",
-        prioridad: "alta"
+        prioridad: "alta",
+        realizada: false,
     },
     {
         id: 6,
         nombre: "Cocinar algo saludable para la semana",
         fecha: "2025-12-15",
-        prioridad: "baja"
+        prioridad: "baja",
+        realizada: false,
     },
     {
         id: 7,
         nombre: "Exposicion del trabajo Grupal",
         fecha: "2025-12-16",
-        prioridad: "alta"
+        prioridad: "alta",
+        realizada: false,
     },
     {
         id: 8,
         nombre: "Ultima Clase Social del Curso MediaPila",
         fecha: "2025-12-17",
-        prioridad: "alta"
+        prioridad: "alta",
+        realizada: false,
     },
     {
         id: 9,
         nombre: "Leer un capítulo de un libro",
         fecha: "2025-12-19",
-        prioridad: "baja"
+        prioridad: "baja",
+        realizada: false,
     },
     {
         id: 10,
         nombre: "Juntarse con las chicas del Grupo 6",
         fecha: "2025-12-22",
-        prioridad: "alta"
+        prioridad: "alta",
+        realizada: false,
     }
 ];
